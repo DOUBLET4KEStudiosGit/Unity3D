c1ba7f13eaf96ae642afff4eda27b3584977f367
DOUBLET4KE Studios <157619209+DOUBLET4KEStudiosGit@users.noreply.github.com>
2024-01-24 18:02:52 -0500

    Update FPSLimiter.cs

--- Scripts/GameManager/FPSLimiter.cs
+++ Scripts/GameManager/FPSLimiter.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class FPSLimiter : MonoBehaviour
 {
     #region Methods
+
+    [SerializeField]
+    int _frameRate = 5f;
     
     // Start is called before the first frame update
     void Start()
     {
         // Set the target frame rate
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = _frameRate;
     }
     
     #endregion Methods
